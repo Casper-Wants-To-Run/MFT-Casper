6d95201a1ce12399e2642aa2587d1d4f8004f078
YeoPEVA <ye0peva@cu.ac.kr>
2024-02-12 21:08:33 +0900

    Qt Test Code

--- mftCasper-GUI.py
+++ mftCasper-GUI.py
@@ -5,4 +5,29 @@ try:
 except:
     from .analyzemft import mftsession
 
+import sys
+from PyQt6.QtWidgets import *
+from PyQt6 import uic
 
+# UI 파일 연결
+form_class = uic.loadUiType("Qt/form.ui")[0]
+
+class WindowClass(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # layout = QVBoxLayout()
+
+        self.pushButton_MFT_location.clicked.connect(self.buttonMFTFunction)
+        self.pushButton_Report_location.clicked.connect(self.buttonMFTFunction)
+
+    def buttonMFTFunction(self):
+        print("MFT Clicked")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec()
